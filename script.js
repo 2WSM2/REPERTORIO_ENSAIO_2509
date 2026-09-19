60f229f56591b5c04c82ee6575e386b299d3b445
Wesley Motta <wesleybr272@gmail.com>
2026-09-19 02:55:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,561 @@
+const SONGS_DATABASE = [
+  {
+    id: "come-as-you-are",
+    title: "1. Come As You Are - Nirvana",
+    notes: "Afinação Original: D Standard (D G C F A D). Usar tom limpo com efeito Chorus.",
+    videoId: "RHMOAId0ky8", // Guitar Cover
+    chords: `Fm5 (F5): 133xxx
+G#5: 466xxx
+F#5: 244xxx
+A5:  x022xx ou 577xxx
+C5:  x355xx
+B5:  x244xx
+D5:  x577xx`,
+    tab: `[Afinação: D Standard (D G C F A D)]
+
+RIFF PRINCIPAL (Intro & Versos):
+d|----------------------------------|
+A|----------------------------------|
+F|----------------------------------|
+C|----------------------------------|
+G|---------0---0---------2----------|
+D|--0-0-1-2---2---2-2-1-0---0-0-----|
+
+[Intro]
+(Riff Principal x4)
+
+[Verso 1]
+(Riff Principal continua durante todo o verso)
+Come as you are, as you were
+As I want you to be
+As a friend, as a friend
+As an old enemy
+
+Take your time, hurry up
+Choice is yours, don't be late
+Take a rest as a friend
+As an old memory... Yeah
+
+[Pré-Refrão]
+F#5                  A5
+Memoria... Yeah
+F#5                  A5
+Memoria... Yeah
+F#5                  A5
+Memoria... Yeah
+
+[Verso 2]
+(Riff Principal)
+Come doused in mud, soaked in bleach
+As I want you to be
+As a trend, as a friend
+As an old memory... Yeah
+
+[Pré-Refrão]
+F#5                  A5
+Memoria... Yeah
+F#5                  A5
+Memoria... Yeah
+F#5                  A5
+Memoria... Yeah
+
+[Refrão]
+B5           D5
+And I swear that I don't have a gun
+B5           D5
+No I don't have a gun
+B5           D5
+No I don't have a gun
+
+[Interlúdio & Solo de Guitarra]
+(Base do solo em F#5 e A5 com pedal Chorus/Small Clone)
+d|----------------------------------|
+A|--12--12--12--12/14-14-14---------|
+F|--11--11--11--11/13-13-13---------|
+C|----------------------------------|  (x8)
+G|----------------------------------|
+D|----------------------------------|
+
+[Ponte]
+F#5                  A5
+Memoria... Yeah
+F#5                  A5
+Memoria... Yeah (No I don't have a gun)
+
+[Outro]
+B5           D5
+And I swear that I don't have a gun
+B5           D5
+No I don't have a gun
+B5           D5
+No I don't have a gun
+B5           D5
+No I don't have a gun
+
+[Final]
+F#5                  A5
+Memoria... Yeah
+(Repetir F#5 A5 até esvanecer)`
+  },
+  {
+    id: "drain-you",
+    title: "2. Drain You - Nirvana",
+    notes: "Afinação Original: D Standard (D G C F A D). Ritmo de Power Chords marcados e distorção pesada no refrão.",
+    videoId: "hmPPlMT8gIE", // Guitar Cover
+    chords: `B5:  x244xx
+D#5: x688xx
+G#5: 466xxx
+C#5: x466xx
+F#5: 244xxx
+A5:  577xxx`,
+    tab: `[Afinação: D Standard (D G C F A D)]
+
+[Intro]
+B5  D#5  G#5  C#5 (x2)
+
+[Verso 1]
+B5        D#5       G#5        C#5
+One baby to another says "I'm lucky to have met you"
+B5        D#5       G#5        C#5
+I don't care what you think unless it is about me
+B5        D#5       G#5        C#5
+It is now my duty to completely drain you
+B5        D#5       G#5        C#5
+I travel through a tube and end up in your infection
+
+[Refrão]
+F#5              A5
+Chew your meat for you
+B5               C#5
+Pass it back and forth
+F#5              A5
+In a passionate kiss
+B5               C#5
+From my mouth to yours
+F#5              A5
+I like you
+B5               C#5
+With my sight line...
+
+[Verso 2]
+B5        D#5       G#5        C#5
+With eyes so dilated I've become your pupil
+B5        D#5       G#5        C#5
+You've taught me everything without a poison apple
+B5        D#5       G#5        C#5
+The water is so yellow it is a healthy sign
+B5        D#5       G#5        C#5
+The baby smells like butter and the air is fine
+
+[Refrão]
+F#5              A5
+Chew your meat for you
+B5               C#5
+Pass it back and forth
+F#5              A5
+In a passionate kiss
+B5               C#5
+From my mouth to yours
+F#5              A5
+Sloppy kiss
+
+[Ponte / Interlúdio Experimental]
+(Segura a nota B5 com feedback/microfonia)
+(Efeitos de ruído, brinquedos estridentes e distorção crescente)
+
+[Explosão da Ponte]
+B5 (Aumentando distorção e ritmo acelerado)
+B5  D#5  G#5  C#5 (x2)
+
+[Verso 3]
+B5        D#5       G#5        C#5
+One baby to another says "I'm lucky to have met you"
+B5        D#5       G#5        C#5
+I don't care what you think unless it is about me
+B5        D#5       G#5        C#5
+It is now my duty to completely drain you
+B5        D#5       G#5        C#5
+I travel through a tube and end up in your infection
+
+[Refrão Final]
+F#5              A5
+Chew your meat for you
+B5               C#5
+Pass it back and forth
+F#5              A5
+In a passionate kiss
+B5               C#5
+From my mouth to yours
+
+[Outro]
+F#5              A5
+Sloppy kiss...
+B5               C#5
+Sloppy kiss...
+F#5              A5
+Sloppy kiss...
+B5               C#5
+(Finalizar com acorde B5 seco e feedback)`
+  },
+  {
+    id: "breed",
+    title: "3. Breed - Nirvana",
+    notes: "Afinação Original: Padrão (E A D G B E). Riff acelerado em F#5 com pausas e pedais de distorção.",
+    videoId: "AyDCs6WbvS0", // Guitar Cover
+    chords: `F#5: 244xxx
+A5:  577xxx
+G5:  355xxx
+D5:  x577xx
+C5:  x355xx
+B5:  x244xx`,
+    tab: `[Afinação: Padrão (E A D G B E)]
+
+[Intro]
+F#5  A5  G5 (x4)
+
+[Verso 1]
+F#5         A5  G5
+I don't care, I don't care, I don't care, I don't care, care
+F#5         A5  G5
+Care if I'm old, I don't mind, I don't mind, I don't mind, mind
+F#5         A5  G5
+Mind if I don't have a mind, Get away, get away, get away, away
+F#5         A5  G5
+Away from your home, I'm afraid, I'm afraid, I'm afraid, afraid
+F#5         A5  G5
+Ghost!
+
+[Refrão]
+D5      C5      B5     A5
+Even if you have, even if you need
+D5      C5      B5     A5
+I don't mean to stare, we don't have to breed
+D5      C5      B5     A5
+We can plant a house, we can build a tree
+D5      C5      B5     A5
+I don't even care, we could have all three
+
+[Ponte 1]
+F#5         A5  G5
+She said, she said, she said, she said
+F#5         A5  G5
+She said, she said, she said, she said
+
+[Verso 2]
+F#5         A5  G5
+I don't care, I don't care, I don't care, I don't care, care
+F#5         A5  G5
+Care if I'm old, I don't mind, I don't mind, I don't mind, mind
+F#5         A5  G5
+Mind if I don't have a mind, Get away, get away, get away, away
+F#5         A5  G5
+Away from your home, I'm afraid, I'm afraid, I'm afraid, afraid
+F#5         A5  G5
+Ghost!
+
+[Refrão]
+D5      C5      B5     A5
+Even if you have, even if you need
+D5      C5      B5     A5
+I don't mean to stare, we don't have to breed
+D5      C5      B5     A5
+We can plant a house, we can build a tree
+D5      C5      B5     A5
+I don't even care, we could have all three
+
+[Solo de Guitarra]
+(Tocado com pedal de Pitch Shifter / Whammy e distorção pesada)
+e|----------------------------------|
+B|--14-14-14-14-14-14-14-14---------|
+G|--14-14-14-14-14-14-14-14---------| (Variações rápidas)
+D|----------------------------------|
+A|----------------------------------|
+E|----------------------------------|
+
+[Outro]
+F#5         A5  G5
+She said, she said, she said, she said
+F#5         A5  G5
+She said, she said, she said, she said
+F#5         A5  G5
+She said, she said...
+F#5 (Sustentar até o final)`
+  },
+  {
+    id: "rape-me",
+    title: "4. Rape Me - Nirvana",
+    notes: "Afinação Original: Padrão (E A D G B E). Inicia limpo/dedilhado e explode com distorção no refrão.",
+    videoId: "wWftoQHoFbc", // Guitar Cover
+    chords: `A5:  577xxx ou x022xx
+C5:  x355xx
+E5:  022xxx
+G5:  355xxx
+C#5: x466xx
+F#5: 244xxx`,
+    tab: `[Afinação: Padrão (E A D G B E)]
+
+[Intro] (Som Limpo / Strumming)
+A5  C5  E5  G5 (x2)
+
+[Verso 1] (Som Limpo)
+A5      C5     E5     G5
+Rape me, rape me my friend
+A5      C5     E5     G5
+Rape me, rape me again
+A5      C5     E5     G5
+I'm not the only one
+A5      C5     E5     G5
+I'm not the only one
+
+[Verso 2] (Com Distorção Pesada)
+A5      C5     E5     G5
+Hate me, hate me do it again
+A5      C5     E5     G5
+Waste me, waste me my friend
+A5      C5     E5     G5
+I'm not the only one
+A5      C5     E5     G5
+I'm not the only one
+
+[Refrão]
+C#5         A5
+My favorite inside source
+F#5         A5
+I'll kiss your open sores
+C#5         A5
+Appreciate your concern
+F#5         G5
+You'll always stink and burn
+
+[Verso 3]
+A5      C5     E5     G5
+Rape me, rape me my friend
+A5      C5     E5     G5
+Rape me, rape me again
+A5      C5     E5     G5
+I'm not the only one
+A5      C5     E5     G5
+I'm not the only one
+
+[Outro - Clímax Vocal & Guitarra]
+A5   C5   E5   G5
+Rape me! Rape me!
+A5   C5   E5   G5
+Rape me! Rape me!
+A5   C5   E5   G5
+Rape me! Rape me!
+A5   C5   E5   G5
+Rape me!
+A5 (Deixar soar com feedback final)`
+  },
+  {
+    id: "follow-you",
+    title: "5. Follow You - BMTH",
+    notes: "Afinação Original: Drop C (C G C F A D). Guitarra base limpa com efeitos de delay/reverb.",
+    videoId: "I47gZoUpl9Q", // Guitar Cover
+    chords: `C5: 000xxx
+F5: 333xxx
+A5: 777xxx
+G5: 555xxx`,
+    tab: `[Afinação: Padrão (Am C F G)]
+
+[Intro]
+F   Am   G  (x2)
+
+[Verso 1]
+F                                           Am              G
+My head is haunting me and my heart feels like a ghost
+F                                       Am              G
+I need to feel something, 'cause I'm still so far from home
+F                     
+Cross your heart and hope to die
+Am                        G               F     Am   G
+Promise me you'll never leave my side 
+
+[Verso 2]
+F                                               Am          G
+Show me what I can't see when the spark in your eyes is gone
+F                                  Am                G
+You've got me on my knees, I'm your one-man cult
+F                   
+Cross my heart and hope to die
+Am               G
+Promise you I'll never leave your side 
+
+[Pré-Refrão]
+Am                       F          G          Am
+'Cause I'm telling you, you're all I need  
+G            C           G          Am
+I promise you you're all I see
+Am                       F          G          Am
+'Cause I'm telling you, you're all I need  
+G        C
+I'll never leave
+
+[Refrão]
+             Am                         F
+So, you can drag me through hell
+             Am                   G
+If it meant I could hold your hand
+             Am                         F
+I will follow you, 'cause I'm under your spell
+             Am                   G
+And you can throw me to the flames
+             Am                     F    G
+I will follow you, I will follow you 
+
+[Verso 3]
+F                                      Am                 G
+Come sink into me and let me breathe you in
+F                                      Am           G
+I'll be your gravity, you be my oxygen
+F                   
+So dig two graves, 'cause when you die 
+Am              G                                 F
+I swear I'll be leaving by your side
+
+[Refrão]
+             Am                         F
+So, you can drag me through hell
+             Am                   G
+If it meant I could hold your hand
+             Am                         F
+I will follow you, 'cause I'm under your spell
+             Am                   G
+And you can throw me to the flames
+             Am                     F    G
+I will follow you, I will follow you`
+  },
+  {
+    id: "can-you-feel-my-heart",
+    title: "6. Can You Feel My Heart - BMTH",
+    notes: "Afinação Original: Drop B (B F# B E G# C#). Riff pesado sintetizado tocado com afinação bem grave na guitarra.",
+    videoId: "epji2J5Q5AA", // Guitar Cover
+    chords: `Em5 (Bm5): 222xxx
+C5:        10 10 10 xxx (10ª casa)
+G5:        555xxx
+D5:        000xxx`,
+    tab: `[Afinação: Padrão]
+
+[Intro / Riff Synth & Guitarra]
+C   Am   Em   G  (x2)
+
+[Verso 1]
+C                       Am
+Can you hear the silence?
+Em                    G
+Can you see the dark?
+C                     Am
+Can you fix the broken?
+Em               G
+Can you feel, can you feel my heart?
+
+[Verso 2]
+C                       Am
+Can you help the hopeless?
+Em                        G
+Well, I'm begging on my knees
+C                       Am
+Can you save my bastard soul?
+Em                 G
+Will you wait for me?
+
+C                       Am
+I'm sorry, brothers, so sorry, lover
+Em                        G
+Forgive me, father, I love you mother
+
+C
+Can you hear the silence?
+Am            
+Can you see the dark?
+Em                 
+Can you fix the broken?
+
+[Refrão]
+G                    C    Am   Em
+Can you feel my heart? 
+G                    C    Am   Em
+Can you feel my heart?
+
+[Ponte]
+C                               Am
+I'm scared to get close, and I hate being alone
+Em                              G
+I long for that feeling to not feel at all
+C                       Am
+The higher I get, the lower I sink
+Em                              G
+I can't drown my demons, they know how to swim`
+  }
+];
+
+class SongApp {
+  constructor(songs) {
+    this.songs = songs;
+    this.activeSongIndex = 0;
+
+    this.elements = {
+      menuContainer: document.getElementById('song-menu'),
+      songTitle: document.getElementById('song-title'),
+      songNotes: document.getElementById('song-notes'),
+      chordDiagrams: document.getElementById('chord-diagrams'),
+      tabDisplay: document.getElementById('tab-display'),
+      youtubePlayer: document.getElementById('youtube-player')
+    };
+  }
+
+  init() {
+    this.renderMenu();
+    this.loadSong(0);
+  }
+
+  renderMenu() {
+    const { menuContainer } = this.elements;
+    if (!menuContainer) return;
+
+    menuContainer.innerHTML = '';
+    const fragment = document.createDocumentFragment();
+
+    this.songs.forEach((song, index) => {
+      const button = document.createElement('button');
+      button.className = 'btn';
+      button.dataset.index = index;
+      button.textContent = song.title.split(' - ')[0];
+      
+      button.addEventListener('click', () => this.loadSong(index));
+      fragment.appendChild(button);
+    });
+
+    menuContainer.appendChild(fragment);
+  }
+
+  loadSong(index) {
+    if (index < 0 || index >= this.songs.length) return;
+
+    this.activeSongIndex = index;
+    const song = this.songs[index];
+
+    this.updateActiveButton(index);
+
+    const { songTitle, songNotes, chordDiagrams, tabDisplay, youtubePlayer } = this.elements;
+
+    if (songTitle) songTitle.textContent = song.title;
+    if (songNotes) songNotes.innerHTML = `<strong>Afinação & Dicas:</strong> ${song.notes}`;
+    if (chordDiagrams) chordDiagrams.textContent = song.chords;
+    if (tabDisplay) tabDisplay.textContent = song.tab;
+    if (youtubePlayer) youtubePlayer.src = `https://www.youtube.com/embed/${song.videoId}`;
+  }
+
+  updateActiveButton(activeIndex) {
+    const buttons = this.elements.menuContainer?.querySelectorAll('.btn');
+    buttons?.forEach((btn, index) => {
+      btn.classList.toggle('active', index === activeIndex);
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new SongApp(SONGS_DATABASE);
+  app.init();
+});
